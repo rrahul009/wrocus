bd7374b4e52e8a09d220489986271f3d2248d4e1
Rahul Yadav <rahulyadav@Rahuls-MacBook-Air.local>
2024-11-27 15:08:59 +0530

    added suspence in useSearchParams

--- src/app/DocViewer/page.js
+++ src/app/DocViewer/page.js
@@ -1,26 +1,44 @@
-'use client'
+'use client';
+
 import AdminAuth from "@/components/AdminAuth";
 import { useSearchParams } from "next/navigation";
 import { useEffect, useState } from "react";
 import { DocumentViewer } from "react-documents";
+import { Suspense } from "react";
 
-export default function DocViwer() {
+export default function DocViewer() {
+    const searchParams = useSearchParams();
+    const [uri, setUri] = useState('');
 
-    const searchParams = useSearchParams()
-    const [uri, seturi] = useState('')
+    useEffect(() => {
+        const fetchResumeData = async () => {
+            const userID = searchParams.get('id');
+            if (userID) {
+                try {
+                    const resumeDetail = await fetch(`http://localhost:5001/api/resume/${userID}`).then((r) => r.json());
+                    const uriResume = resumeDetail?.data.secure_url ?? '';
+                    setUri(uriResume);
+                } catch (error) {
+                    console.error("Error fetching resume:", error);
+                }
+            }
+        };
 
-    useEffect(async () => {
-        const userID = searchParams.get('id')
-        const resumeDetail = await fetch(`http://localhost:5001/api/resume/${userID}`).then((r) => r.json())
-        const uriResume = resumeDetail?.data.secure_url ?? '';
-        seturi(uriResume)
-    }, [])
+        fetchResumeData();
+    }, [searchParams]); // Dependencies array, only re-run when searchParams change
 
     return (
-        <div className="App">
-            <AdminAuth>
-                <DocumentViewer style={{ height: '100vh', width: '100%' }} queryParams="hl=EN" url={uri} overrideLocalhost='' />
-            </AdminAuth>
-        </div>
+        <Suspense fallback={<div>Loading...</div>}>
+            <div className="App">
+                <AdminAuth>
+                    <DocumentViewer
+                        style={{ height: '100vh', width: '100%' }}
+                        queryParams="hl=EN"
+                        url={uri}
+                        overrideLocalhost=''
+                    />
+                </AdminAuth>
+            </div>
+        </Suspense>
     );
 }
