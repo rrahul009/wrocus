1705347db982c10421dea98a1fc58712a7b82543
Rahul Yadav <rahulyadav@Rahuls-MacBook-Air.local>
2024-11-27 15:16:04 +0530

    useSearchParam

--- src/app/DocViewer/page.js
+++ src/app/DocViewer/page.js
@@ -1,44 +1,53 @@
-'use client';
+// 'use client';
 
-import { useSearchParams } from 'next/navigation';
-import { useEffect, useState, Suspense } from 'react';
-import AdminAuth from '@/components/AdminAuth';
-import { DocumentViewer } from 'react-documents';
+// import { useSearchParams } from 'next/navigation';
+// import { useEffect, useState, Suspense } from 'react';
+// import AdminAuth from '@/components/AdminAuth';
+// import { DocumentViewer } from 'react-documents';
 
-export default function DocViewer() {
-    const searchParams = useSearchParams();
-    const [uri, setUri] = useState('');
+// export default function DocViewer() {
+//     const searchParams = useSearchParams();
+//     const [uri, setUri] = useState('');
 
-    useEffect(() => {
-        const fetchResumeData = async () => {
-            const userID = searchParams.get('id');
-            if (userID) {
-                try {
-                    const response = await fetch(`http://localhost:5001/api/resume/${userID}`);
-                    const resumeDetail = await response.json();
-                    const uriResume = resumeDetail?.data.secure_url ?? '';
-                    setUri(uriResume);
-                } catch (error) {
-                    console.error("Error fetching resume:", error);
-                }
-            }
-        };
+//     useEffect(() => {
+//         const fetchResumeData = async () => {
+//             const userID = searchParams.get('id');
+//             if (userID) {
+//                 try {
+//                     const response = await fetch(`http://localhost:5001/api/resume/${userID}`);
+//                     const resumeDetail = await response.json();
+//                     const uriResume = resumeDetail?.data.secure_url ?? '';
+//                     setUri(uriResume);
+//                 } catch (error) {
+//                     console.error("Error fetching resume:", error);
+//                 }
+//             }
+//         };
 
-        fetchResumeData();
-    }, [searchParams]);
+//         fetchResumeData();
+//     }, [searchParams]);
 
-    return (
-        <Suspense fallback={<div>Loading...</div>}>
-            <div className="App">
-                <AdminAuth>
-                    <DocumentViewer
-                        style={{ height: '100vh', width: '100%' }}
-                        queryParams="hl=EN"
-                        url={uri}
-                        overrideLocalhost=''
-                    />
-                </AdminAuth>
-            </div>
-        </Suspense>
-    );
+//     return (
+//         <Suspense fallback={<div>Loading...</div>}>
+//             <div className="App">
+//                 <AdminAuth>
+//                     <DocumentViewer
+//                         style={{ height: '100vh', width: '100%' }}
+//                         queryParams="hl=EN"
+//                         url={uri}
+//                         overrideLocalhost=''
+//                     />
+//                 </AdminAuth>
+//             </div>
+//         </Suspense>
+//     );
+// }
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>page</div>
+  )
 }
+
+export default page
--- src/app/appliedjob/page.js
+++ src/app/appliedjob/page.js
@@ -114,13 +114,13 @@ const page = () => {
                                                     <path fillRule="evenodd" d="M9.657 15.874 7.358 13H5a2 2 0 0 0-2 2v4a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-4a2 2 0 0 0-2-2h-2.358l-2.3 2.874a3 3 0 0 1-4.685 0ZM17 16a1 1 0 1 0 0 2h.01a1 1 0 1 0 0-2H17Z" clipRule="evenodd" />
                                                 </svg>
                                             </button>
-                                            <Link target='_blank' href={`/DocViewer/?id=${item?._id}`} >
+                                            {/* <Link target='_blank' href={`/DocViewer/?id=${item?._id}`} >
                                                 <svg className="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
                                                     <path stroke="currentColor" strokeWidth="2" d="M21 12c0 1.2-4.03 6-9 6s-9-4.8-9-6c0-1.2 4.03-6 9-6s9 4.8 9 6Z" />
                                                     <path stroke="currentColor" strokeWidth="2" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
                                                 </svg>
 
-                                            </Link>
+                                            </Link> */}
                                             
                                                <button onClick={()=>handleDelete(item._id)}>
                                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
